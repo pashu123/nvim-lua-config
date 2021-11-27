51c1fa9ce6e505f920fa6825f0fe3fd39290c1cd
Prashant Kumar <prashant@nod-labs.com>
2021-11-27 17:58:02 +0000

    Add some changes to telescope buffers.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -8,6 +8,14 @@
 local map = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
 
+local actions = require('telescope.actions')require('telescope').setup{
+  pickers = {
+    buffers = {
+      sort_lastused = true
+    }
+  }
+}
+
 map('n', '<leader>,f', ':Telescope current_buffer_fuzzy_find<CR>', default_opts)
 map('n', '<C-f>', ':Telescope find_files<CR>', default_opts)
 map('n', '<leader>ff', ':Telescope live_grep<CR>', default_opts)
