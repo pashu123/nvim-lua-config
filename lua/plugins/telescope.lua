a9989fb533e2ba7b0f38abca1f1f4eac1b6b01b1
Prashant Kumar <prashant@nod-labs.com>
2021-11-30 13:26:13 +0530

    Added enhancements to telescope plugin.
    Telescope now supports smart case etc.
    This is borrowed from lunarVim.

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -13,7 +13,25 @@ local actions = require('telescope.actions')require('telescope').setup{
     buffers = {
       sort_lastused = true
     }
-  }
+  },
+  vimgrep_arguments = {
+    "rg",
+    "--color=never",
+    "--no-heading",
+    "--with-filename",
+    "--line-number",
+    "--column",
+    "--smart-case",
+    "--hidden",
+  },
+  extensions = {
+      fzf = {
+        fuzzy = true, -- false will only do exact matching
+        override_generic_sorter = true, -- override the generic sorter
+        override_file_sorter = true, -- override the file sorter
+        case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+      },
+    },
 }
 
 map('n', '<leader>,f', ':Telescope current_buffer_fuzzy_find<CR>', default_opts)
